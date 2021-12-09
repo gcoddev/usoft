419ea1d8e43ae7086a5314ae00016725ee21695b
Gary <geco.yak77@gmail.com>
2021-12-08 23:21:34 -0400

    uSoft v1.2

---
+++ app.js
@@ -0,0 +1,27 @@
+let udev = () => {
+    document.getElementById('fondo').style.display = 'flex';
+    document.getElementById('udev').style.display = 'flex'
+}
+
+let uedu = () => {
+    document.getElementById('fondo').style.display = 'flex';
+    document.getElementById('uedu').style.display = 'flex'
+}
+
+let ucloud = () => {
+    document.getElementById('fondo').style.display = 'flex';
+    document.getElementById('ucloud').style.display = 'flex'
+}
+
+let usec = () => {
+    document.getElementById('fondo').style.display = 'flex';
+    document.getElementById('usec').style.display = 'flex'
+}
+
+let cerrar = () => {
+    document.getElementById('fondo').style.display = 'none';
+    document.getElementById('udev').style.display = 'none'
+    document.getElementById('uedu').style.display = 'none'
+    document.getElementById('ucloud').style.display = 'none'
+    document.getElementById('usec').style.display = 'none'
+}
